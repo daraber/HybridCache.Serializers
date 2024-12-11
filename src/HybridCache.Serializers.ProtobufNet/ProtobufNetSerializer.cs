69bf9c39bbbf4f3134230e4705661465c4d5c496
David <da6003be-s@student.lu.se>
2024-12-11 21:17:53 +0100

    Implement protobuf-net serializer

---
+++ src/HybridCache.Serializers.ProtobufNet/ProtobufNetSerializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Buffers;
+using Microsoft.Extensions.Caching.Hybrid;
+using ProtoBuf;
+
+namespace HybridCache.Serializers.ProtobufNet;
+
+public class ProtobufNetSerializer<T> : IHybridCacheSerializer<T>
+{
+    public T Deserialize(ReadOnlySequence<byte> source)
+    {
+        return Serializer.Deserialize<T>(source);
+    }
+
+    public void Serialize(T value, IBufferWriter<byte> target)
+    {
+        Serializer.Serialize(target, value);
+    }
+}
