160ab6dac37c1e5756f59d5bf6d31e236450b55f
David <da6003be-s@student.lu.se>
2024-12-11 21:24:27 +0100

    Add protobuf serializer tests

---
+++ tests/HybridCache.Serializers.ProtobufNet.Tests/ProtoContractDto.cs
@@ -0,0 +1,27 @@
+﻿using ProtoBuf;
+
+namespace HybridCache.Serializers.ProtobufNet.Tests;
+
+[ProtoContract]
+internal sealed record ProtoContractDto
+{
+    [ProtoMember(1)] public required string Field1 { get; init; }
+    [ProtoMember(2)] public required int Field2 { get; init; }
+    [ProtoMember(3)] public required bool Field3 { get; init; }
+    [ProtoMember(4)] public required double Field4 { get; init; }
+    [ProtoMember(5)] public required (string, bool) Field5 { get; init; }
+    
+    public static ProtoContractDto Random()
+    {
+        var rand = new Random();
+
+        return new ProtoContractDto
+        {
+            Field1 = Guid.NewGuid().ToString(),
+            Field2 = rand.Next(),
+            Field3 = rand.Next() % 2 == 0,
+            Field4 = rand.NextDouble(),
+            Field5 = (Guid.NewGuid().ToString(), rand.Next() % 2 == 0)
+        };
+    }
+}
---
+++ tests/HybridCache.Serializers.ProtobufNet.Tests/ProtobufNet.Tests.cs
@@ -0,0 +1,30 @@
+﻿using System.Buffers;
+
+namespace HybridCache.Serializers.ProtobufNet.Tests;
+
+[TestOf(typeof(ProtobufNetSerializer<>))]
+[Category("ProtobufNet")]
+[Parallelizable(ParallelScope.All)]
+public class ProtobufNetTests
+{
+    private ProtobufNetSerializer<ProtoContractDto> _memoryPackSerializer;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _memoryPackSerializer = new ProtobufNetSerializer<ProtoContractDto>();
+    }
+
+    [Test]
+    public void MemoryPackSerializer_SerializeAndDeserialize_ShouldBeEqual()
+    {
+        var dto = ProtoContractDto.Random();
+        var target = new ArrayBufferWriter<byte>();
+        _memoryPackSerializer.Serialize(dto, target);
+
+        Assert.That(target.WrittenMemory.Length, Is.GreaterThan(0), "Serializer should write data");
+
+        var deserializedDto = _memoryPackSerializer.Deserialize(new ReadOnlySequence<byte>(target.WrittenMemory));
+        Assert.That(dto, Is.EqualTo(deserializedDto));
+    }
+}
